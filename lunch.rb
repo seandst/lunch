0d98cc4c34bbad2ea7cbe6a387d2d400fa1a54ad
Adam Price <komidore64@gmail.com>
2013-09-24 13:26:34 -0400

    first go-round of lunch.rb

---
+++ lunch.rb
@@ -0,0 +1,44 @@
+# lunch.rb - where should we go to lunch today?
+# Copyright (C) 2013  M. Adam Price
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see {http://www.gnu.org/licenses/}.
+
+PLACES = [
+  "battistella's",
+  "beasley's",
+  "bida manda",
+  "the big easy",
+  "big ed's",
+  "buku",
+  "busy bee",
+  "capital club 16",
+  "centro",
+  "chuck's",
+  "cooper's",
+  "el rodeo",
+  "garland",
+  "gravy",
+  "mantra",
+  "mecca",
+  "oxford",
+  "the pit",
+  "raleigh times",
+  "remedy diner",
+  "sitti",
+  "sono",
+  "spize",
+  "tir na nog",
+]
+
+puts PLACES[rand(PLACES.size)]
