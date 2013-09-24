d81d3606358993e1ecf29a2c49c1b2cfaa5ecd9a
Sean Myers <sean.myers@redhat.com>
2013-09-24 15:36:07 -0400

    Bashing it up in here

--- lunch.sh
+++ lunch.sh
@@ -1,5 +1,5 @@
-#!/usr/bin/env python
-# lunch.py - where should we go to lunch today?
+#!/bin/bash
+# lunch.sh - where should we go to lunch today?
 # Copyright (C) 2013  M. Adam Price
 #
 # This program is free software: you can redistribute it and/or modify
@@ -7,41 +7,40 @@
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# This program is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see {http://www.gnu.org/licenses/}.
-import random
 
-PLACES = [
-  "battistella's",
-  "beasley's",
-  "bida manda",
-  "the big easy",
-  "big ed's",
-  "buku",
-  "busy bee",
-  "capital club 16",
-  "centro",
-  "chuck's",
-  "cooper's",
-  "el rodeo",
-  "garland",
-  "gravy",
-  "mantra",
-  "mecca",
-  "oxford",
-  "the pit",
-  "raleigh times",
-  "remedy diner",
-  "sitti",
-  "sono",
-  "sosta cafe",
-  "spize",
-  "tir na nog",
-]
+PLACES=(
+  "battistella's"
+  "beasley's"
+  "bida manda"
+  "the big easy"
+  "big ed's"
+  "buku"
+  "busy bee"
+  "capital club 16"
+  "centro"
+  "chuck's"
+  "cooper's"
+  "el rodeo"
+  "garland"
+  "gravy"
+  "mantra"
+  "mecca"
+  "oxford"
+  "the pit"
+  "raleigh times"
+  "remedy diner"
+  "sitti"
+  "sono"
+  "sosta cafe"
+  "spize"
+  "tir na nog"
+)
 
-print random.choice(PLACES)
+echo ${PLACES[`seq ${#PLACES[@]} | sort -R | head -n 1 | xargs -I{} expr {} - 1`]}
